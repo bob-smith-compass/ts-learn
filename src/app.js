b7f20f83770dcd5b56d83e532a56d227d951cc3d
David <david.shams@gmail.com>
2019-08-22 11:17:05 -0500

    showOutput args NOT WORKING

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 var message = "TypeScript 3.5.1";
+debugger;
 console.log(message);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 
+
 const message: string = "TypeScript 3.5.1";
+
+debugger;
 
 console.log(message);
